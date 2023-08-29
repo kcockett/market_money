2009910f1748937f3833bd43fc89f8a2f135256c
Kaina Cockett <kainacockett@gmail.com>
2023-08-29 08:15:27 -0600

    complete story 2

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,4 +1,5 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
     markets = Market.all
@@ -8,6 +9,15 @@ class Api::V0::MarketsController < ApplicationController
   end
 
   def show
+    market = [Market.find(params[:id])]
+    facade = MarketsFacade.new(market)
+    formatted_data = { data: facade.formatted_data }
+    render json: formatted_data
+  end
 
+  private
+  
+  def record_not_found
+    render json: { "errors": [{"detail" => "Could not find Market with 'id'=#{params[:market_id]}"}] }, status: :not_found
   end
 end
--- spec/requests/api/v0/market_spec.rb
+++ spec/requests/api/v0/market_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Markets", type: :request do
-  describe "GET /api/v0/markets" do
+  describe "1. GET /api/v0/markets" do
     it "should return a list of all markets in the database" do
       market_list = create_list(:market,4)
       get "/api/v0/markets"
       response_data = JSON.parse(response.body, symbolize_names: true)
       markets = response_data[:data]
-
+      
       expect(response).to be_successful
       expect(markets.size).to eq(4)
       markets.each do |market|
@@ -37,10 +37,60 @@ RSpec.describe "Markets", type: :request do
 
         expect(market[:attributes]).to have_key(:lon)
         expect(market[:attributes][:lon]).to be_a(String)
+
+        expect(market[:attributes]).to have_key(:vendor_count)
+        expect(market[:attributes][:vendor_count]).to be_an(Integer)
+        expect(market[:attributes][:vendor_count]).to eq(0)
       end
     end
   end
-  describe "GET /api/v0/markets/:id/vendors" do
+
+  describe "2. GET /api/v0/markets/:id" do
+    it "should return a single market by id" do
+      market = create(:market)
+      vendors = create_list(:vendor,4)
+      vendors.each do |vendor|
+        MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
+      end
+      get "/api/v0/markets/#{market.id}"
+      response_data = JSON.parse(response.body, symbolize_names: true)
+      market_found = response_data[:data]
+
+      expect(response).to be_successful
+      expect(market_found[:attributes][:vendor_count]).to eq(4)
+      expect(market_found).to have_key(:id)
+      expect(market_found[:id]).to be_an(String)
+
+      expect(market_found[:attributes]).to have_key(:name)
+      expect(market_found[:attributes][:name]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:street)
+      expect(market_found[:attributes][:street]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:city)
+      expect(market_found[:attributes][:city]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:county)
+      expect(market_found[:attributes][:county]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:state)
+      expect(market_found[:attributes][:state]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:zip)
+      expect(market_found[:attributes][:zip]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:lat)
+      expect(market_found[:attributes][:lat]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:lon)
+      expect(market_found[:attributes][:lon]).to be_a(String)
+
+      expect(market_found[:attributes]).to have_key(:vendor_count)
+      expect(market_found[:attributes][:vendor_count]).to eq(4)
+    end
+  end
+
+  describe "3. GET /api/v0/markets/:id/vendors" do
     it "should return a list of all vendors in a given market" do
       market = create(:market)
       vendors = create_list(:vendor,4)
@@ -50,6 +100,7 @@ RSpec.describe "Markets", type: :request do
       get "/api/v0/markets/#{market.id}/vendors"
       response_data = JSON.parse(response.body, symbolize_names: true)
       vendors_found = response_data[:data]
+      
 
       expect(response).to be_successful
       expect(vendors_found.size).to eq(4)
@@ -58,7 +109,7 @@ RSpec.describe "Markets", type: :request do
         expect(vendor[:id]).to be_an(String)
 
         expect(vendor[:attributes]).to have_key(:name)
-        expect(vendor[:attributes][:name]).to be_a(String)
+        expect(vendor[:attributes][:name]).to be_an(String)
 
         expect(vendor[:attributes]).to have_key(:description)
         expect(vendor[:attributes][:description]).to be_a(String)
