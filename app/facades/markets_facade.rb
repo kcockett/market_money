49d507bcade5716fc2b37e15b05cdbb299b843d4
Kaina Cockett <kainacockett@gmail.com>
2023-09-01 10:11:14 -0600

    refac: remove unneeded code in vendors_facade

--- app/facades/markets_facade.rb
+++ app/facades/markets_facade.rb
@@ -7,47 +7,6 @@ class MarketsFacade
     @status = :internal_server_error
   end
 
-  
-
-  def array_format
-    @markets.map do |market|
-      {
-        id: market.id.to_s,
-        type: 'market',
-        attributes: {
-          name: market.name,
-          street: market.street,
-          city: market.city,
-          county: market.county,
-          state: market.state,
-          zip: market.zip,
-          lat: market.lat,
-          lon: market.lon,
-          vendor_count: market.vendors.count
-        }
-      }
-    end
-  end
-
-  def single_format
-    market = @markets[0]
-    {
-      id: market.id.to_s,
-      type: 'market',
-      attributes: {
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon,
-        vendor_count: market.vendors.count
-      }
-    }
-  end
-
   def fetch_nearest_atms
     latitude = @market.lat
     longitude = @market.lon
